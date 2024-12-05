a58338969ec069e501de8599d4fd40b59ed6a48e
Moncayo1329 <86531347+Moncayo1329@users.noreply.github.com>
2024-12-05 14:56:06 +0000

    adding first steps

---
+++ src/data/film.js
@@ -0,0 +1,42 @@
+export const Filmphotos = [ 
+    
+{
+
+id:'1',
+img:require('../data/photos/Mike.jpg'),
+description:"me",
+dates:" younger etiqueta"
+
+
+
+
+},
+
+{
+
+    id:'2',
+    img: require('../data/photos/me.jpg'),
+    description:"me and my best friend",
+    dates:" younger etiqueta"
+    
+    
+    
+    
+    },
+
+    {
+
+        id:'3',
+        img: require('../data/photos/Friends.jpg'),
+        description:"me and my best friend",
+        dates:" younger etiqueta"
+        
+        
+        
+        
+        },
+    
+
+
+]
+
